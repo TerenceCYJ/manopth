5417cfa499254d885199a09220f99665bbda9c76
Yujin Chen <yujin.chen@whu.edu.cn>
2020-12-05 15:07:19 +0800

    Update serialization.py

--- mano/webuser/serialization.py
+++ mano/webuser/serialization.py
@@ -28,6 +28,28 @@ from chumpy.ch import MatVecMult
 from mano.webuser.posemapper import posemap
 from mano.webuser.verts import verts_core
 
+def backwards_compatibility_replacements(dd):
+    # replacements
+    if 'default_v' in dd:
+        dd['v_template'] = dd['default_v']
+        del dd['default_v']
+    if 'template_v' in dd:
+        dd['v_template'] = dd['template_v']
+        del dd['template_v']
+    if 'joint_regressor' in dd:
+        dd['J_regressor'] = dd['joint_regressor']
+        del dd['joint_regressor']
+    if 'blendshapes' in dd:
+        dd['posedirs'] = dd['blendshapes']
+        del dd['blendshapes']
+    if 'J' not in dd:
+        dd['J'] = dd['joints']
+        del dd['joints']
+
+    # defaults
+    if 'bs_style' not in dd:
+        dd['bs_style'] = 'lbs'
+
 def ready_arguments(fname_or_dict):
 
     if not isinstance(fname_or_dict, dict):
